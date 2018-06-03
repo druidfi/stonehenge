7ca8fdd78a2eb2bf0a8bbc4d9900d36100114e71
back-2-95 <marko.korhonen@druid.fi>
2018-06-03 10:07:44 +0300

    git clone

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-main () {
-    echo "$(whoami):$(~) and $(pwd)"
+REPO_FOLDER=~/druid-stonehange
+REPO_URL=git@github.com:druidfi/stonehenge.git
 
+main () {
     if [ "$(uname)" == "Darwin" ]; then
         echo "$(uname) detected..."
     else
         echo "Something else detected: $(uname)"
     fi
+
+    if [ ! -d "$REPO_FOLDER" ] ; then
+        git clone $REPO_URL $REPO_FOLDER
+    fi
+
+    cd "$REPO_FOLDER"
+    git pull
+
+    make up
 
     exit 0
 }
