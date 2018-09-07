3bfcf60f792335ee4581ce134fa4a54dbec90d29
back-2-95 <marko.korhonen@druid.fi>
2018-09-07 11:10:39 +0300

    install.sh and multi os testing

---
+++ .travis/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+
+    echo "Hello osx"
+else
+
+    echo "Hello linux"
+
+    # Install latest Docker
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get -y install docker-ce
+
+    # Install docker-compose
+    sudo rm /usr/local/bin/docker-compose
+    curl -L $DOCKER_COMPOSE_REPO/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > docker-compose
+    chmod +x docker-compose
+    sudo mv docker-compose /usr/local/bin
+
+fi
