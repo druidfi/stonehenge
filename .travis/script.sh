51a7669c43a0b813f95edb0c1cc5210b475e581b
Marko Korhonen <marko.korhonen@druid.fi>
2019-09-17 11:47:09 +0300

    Comments to test

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+echo "Distribution: ${TRAVIS_DIST}"
+
 make -v
 docker --version
 docker-compose --version
-
 make debug
+
+# Start up Stonehenge
 make up
+
+# Install mkcert
 make mkcert-install
+
+# Install SSL-sertificate
 sudo make certs
 
+# Check that we can connect to local Portainer
 curl -Is https://portainer.docker.sh | head -1
 
+# Tear down Stonehenge
 make down
 
+# Check that DNS work still
 curl -Is https://www.google.com | head -1
