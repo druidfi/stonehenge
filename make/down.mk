584b6651bf69c9eb3abe146defbadc81f42e659a
Marko Korhonen <marko.korhonen@druid.fi>
2019-09-17 09:27:48 +0300

    Break down down to multiple targets

--- make/down.mk
+++ make/down.mk
@@ -1,12 +1,29 @@
+DOWN_TARGETS := --down-title --down --down-remove-network --down-remove-volume --down-post-actions
+
 RESOLV_CONF := /etc/resolv.conf
 RESOLV_CONF_BAK_EXISTS := $(shell test -f ${RESOLV_CONF}.bak && echo "yes" || echo "no")
 
 PHONY += down
-down: ## Tear down Stonehenge
-	$(call step,Tear down Stonehenge\n\nStop and remove the containers...)
+down: $(DOWN_TARGETS) ## Tear down Stonehenge
+
+PHONY += --down-title
+--down-title:
+	$(call step,Tear down Stonehenge on $(OS))
+
+PHONY += --down
+--down:
 	@${DOCKER_COMPOSE_CMD} down -v --remove-orphans
+
+PHONY += --down-remove-network
+--down-remove-network:
 	@docker network remove ${NETWORK_NAME} || docker network inspect ${NETWORK_NAME}
+
+PHONY += --down-remove-volume
+--down-remove-volume:
 	@docker volume remove ${SSH_VOLUME_NAME} || docker volume inspect ${SSH_VOLUME_NAME}
+
+PHONY += --down-post-actions
+--down-post-actions:
 	$(call step,Remove resolver file...)
 ifeq ($(OS),Darwin)
 	@sudo rm -f "/etc/resolver/${DOCKER_DOMAIN}" && echo "Resolver file removed" || echo "Already removed"
