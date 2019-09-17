d9be537eb51b844be81c3e932c58222ed27d2375
Marko Korhonen <marko.korhonen@druid.fi>
2019-09-17 11:41:33 +0300

    sudo for ubuntu 16.04

--- .travis/script.sh
+++ .travis/script.sh
@@ -7,6 +7,10 @@ docker-compose --version
 make debug
 make up
 make mkcert-install
-make certs
+sudo make certs
 
 curl -Is https://portainer.docker.sh | head -1
+
+make down
+
+curl -Is https://www.google.com | head -1
