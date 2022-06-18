c7f7e8f1853c1675b05616128c3adaf87d5a5533
Marko Korhonen <marko.korhonen@druid.fi>
2022-06-18 21:24:44 +0300

    Push images

--- make/docker.mk
+++ make/docker.mk
@@ -1,4 +1,9 @@
 PHONY += build-docker-image
-build-docker-image: ## Build Stonehenge Traefik Docker image
-	$(call step,Build Stonehenge Traefik Docker image...)
+build-docker-image: ## Build Stonehenge Docker image
+	$(call step,Build Stonehenge Docker image...)
 	@docker buildx bake -f docker-bake.hcl --pull --progress plain --no-cache --load --set *.platform=linux/arm64
+
+PHONY += push-docker-image
+push-docker-image: ## Build and push Stonehenge Docker image
+	$(call step,Build Stonehenge Docker image...)
+	@docker buildx bake -f docker-bake.hcl --pull --no-cache --push
