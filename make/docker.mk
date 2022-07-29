74bcfaa262df05a5e139de9aae736e4e9ed65d20
Marko Korhonen <marko.korhonen@druid.fi>
2022-07-29 13:32:41 +0300

    docker prefix for make targets

--- make/docker.mk
+++ make/docker.mk
@@ -1,9 +1,9 @@
-PHONY += build-docker-image
-build-docker-image: ## Build Stonehenge Docker image
-	$(call step,Build Stonehenge Docker image...)
+PHONY += docker-build-image
+docker-build-image: ## Build Stonehenge Docker image locally
+	$(call step,Build Stonehenge Docker image locally...)
 	@docker buildx bake -f docker-bake.hcl --pull --progress plain --no-cache --load --set *.platform=linux/arm64
 
-PHONY += push-docker-image
-push-docker-image: ## Build and push Stonehenge Docker image
-	$(call step,Build Stonehenge Docker image...)
+PHONY += docker-push-image
+docker-push-image: ## Build and push Stonehenge Docker image
+	$(call step,Build and push Stonehenge Docker image...)
 	@docker buildx bake -f docker-bake.hcl --pull --no-cache --push
